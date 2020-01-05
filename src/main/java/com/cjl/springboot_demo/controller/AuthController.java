f15f66f487213c82d80c3924fc430ea075078cad
Confession <2388699527@qq.com>
2020-01-05 12:32:33 +0800

    add session

--- src/main/java/com/cjl/springboot_demo/controller/AuthController.java
+++ src/main/java/com/cjl/springboot_demo/controller/AuthController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 通过github授权登录
  */
@@ -33,7 +35,8 @@ public class AuthController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirect_uri);
@@ -43,8 +46,16 @@ public class AuthController {
         try {
             String accessToken = githubProvider.getAccessToken(accessTokenDTO);
             GithubUser user=githubProvider.getUser(accessToken);
-            System.out.println(user);
-            return "index";
+            //System.out.println(user);
+            if (user!=null){
+                //登陆成功，写cookie和session
+                request.getSession().setAttribute("user",user);
+                //System.out.println(request.getSession().getAttribute("user"));
+                return "redirect:/";
+            }else {
+                //登录失败，重新登录
+                return "redirect:/";
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
